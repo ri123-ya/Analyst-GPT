fbfcf58cab0c5a9024b34d91a9c8094cf44a4e6b
Riya Rastogi <riya02rastogi@gmail.com>
2025-11-14 13:08:11 +0530

    Added routes

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from "./pages/Register";
+import ChatUI from './pages/chatUI';
+
+function App() {
+
+  return (
+    
+    <Routes>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/register' element={<Register/>}/>
+      <Route path='/chat' element={<ChatUI/>}/>
+    </Routes>
+    
+  )
+}
+
+export default App
