78f5a5ad771755ca960043789262be83b9e27b1d
Riya Rastogi <riya02rastogi@gmail.com>
2025-11-15 18:49:09 +0530

    added middleware to check the user uploading the file

--- backend/routes/uploadRoute.js
+++ backend/routes/uploadRoute.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { fileUpload } from "../controllers/uploadController.js";
+import { verifyToken, verifyChild } from "../middleware/verifyToken.js";
+
 import multer from "multer";
 
 const router = express.Router();
 
 const upload = multer();
 
-router.post("/file", upload.single("file"), fileUpload);
+router.post("/file", verifyToken, verifyChild, upload.single("file"), fileUpload);
 
 export default router;
