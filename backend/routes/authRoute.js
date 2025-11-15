6585475d0e025fedaf37e4b7ccdfb3585cad10e4
Riya Rastogi <riya02rastogi@gmail.com>
2025-11-15 15:44:27 +0530

    added route to register admin

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { login, logout, registerUser } from "../controllers/authController.js";
+import { login, logout, registerUser , registerAdmin} from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/register/user", registerUser);
+router.post("/register/admin", registerAdmin);
 router.post("/login", login);
-router.post("/logout", logout);
+router.get("/logout", logout);
 
 export default router;
